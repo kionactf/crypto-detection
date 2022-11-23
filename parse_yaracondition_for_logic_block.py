1b1c7bfede7133f097ed823bed64df92f4a4bc96
kiona <kiona.ctf@gmail.com>
2022-11-23 21:25:00 +0900

    some cleanup with tools

--- parse_yaracondition_for_logic_block.py
+++ parse_yaracondition_for_logic_block.py
@@ -22,59 +22,68 @@ import re
 from ply.lex import lex
 from ply.yacc import yacc
 
+
 def parse_yaracondition_for_logic_block(parse_target):
     var_lex_pos = []
 
     reserved = {
-        'and' : 'AND',
-        'or'  : 'OR',
-        'not' : 'NOT',
-        'for' : 'FOR',
+        "and" : "AND",
+        "or"  : "OR",
+        "not" : "NOT",
+        "for" : "FOR",
     }
-    
-    tokens = ['LPAREN', 'RPAREN', 'COLON', 'NEQUALS'] +  list(reserved.values()) + ['ID', 'STRINGNAME', 'STRINGNAME_COUNT', 'STRINGNAME_ARRAY', 'STRINGNAME_LENGTH'] 
-    
+
+    tokens = \
+        [
+            'LPAREN', 'RPAREN', 'COLON', 'NEQUALS'
+        ] +\
+        list(reserved.values()) +\
+        [
+            'ID', 'STRINGNAME', 'STRINGNAME_COUNT', 'STRINGNAME_ARRAY',
+            'STRINGNAME_LENGTH'
+        ]
+
     t_ignore = ' \t'
-    
+
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
     t_COLON = r':'
     t_NEQUALS = r'!='
-    
+
     def t_ID(t):
         r'[a-zA-Z0-9._\[\]~*\\%+\-<>&\^|=,]+'
         t.type = reserved.get(t.value, 'ID')  # Check for reserved words
         return t
-    
+
     def t_STRINGNAME(t):
         r'\$[0-9a-zA-Z\-_]*[*]?'
         return t
-    
+
     def t_STRINGNAME_COUNT(t):
         r'\#([a-zA-Z][0-9a-zA-Z\-_]*[*]?)?'
         return t
-    
+
     def t_STRINGNAME_ARRAY(t):
         r'@[0-9a-zA-Z\-_]*[*]?'
         return t
-    
+
     def t_STRINGNAME_LENGTH(t):
         r'![0-9a-zA-Z\-_]*[*]?(?!=)'
         return t
-    
+
     def t_error(t):
         print(f'Illegal character {t.value[0]!r}')
         t.lexer.skip(1)
-    
+
     lexer = lex()
-    
+
     # it seems that we have to define first for parsing end state
     def p_expression(p):
         '''
         expression : term
         '''
         p[0] = p[1]
-    
+
     def p_logic(p):
         '''
         term       : term AND term
@@ -85,33 +94,36 @@ def parse_yaracondition_for_logic_block(parse_target):
             p[0] = p[1] + [p[2]] + p[3]
         elif len(p) == 3:
             p[0] = [p[1]] + p[2]
-    
+
     def p_colon(p):
         '''
         term        : FOR term COLON term
         '''
         p[0] = [p[1]] + p[2] + [p[3]] + p[4]
-    
+
     def p_id(p):
         '''
         ids : ID
             | NEQUALS
         '''
         p[0] = [p[1]]
-    
+
     def p_ids_double(p):
         '''
         ids : ids ids
         '''
         # for expression like '!ddd[0]'
-        p[0] = [re.sub(r'(@|!)([a-zA-Z0-9]+) \[', r'\1\2[', p[1][0] + ' ' + p[2][0])]
-    
+        p[0] = [re.sub(
+            r'(@|!)([a-zA-Z0-9]+) \[',
+            r'\1\2[', p[1][0] + ' ' + p[2][0]
+            )]
+
     def p_ids_term(p):
         '''
         term : ids
         '''
         p[0] = p[1]
-    
+
     def p_var(p):
         '''
         ids : STRINGNAME
@@ -121,19 +133,19 @@ def parse_yaracondition_for_logic_block(parse_target):
         '''
         var_lex_pos.append((p[1], p.lexpos(1)))
         p[0] = [p[1]]
-    
+
     def p_grouped(p):
         '''
         term : LPAREN term RPAREN
         '''
         p[0] = [p[1]] + p[2] + [p[3]]
-    
+
     def p_notgrouped(p):
         '''
         ids : LPAREN ids RPAREN
         '''
         p[0] = [p[1] + ' ' + p[2][0] + ' ' + p[3]]
-    
+
     def p_error(p):
         print(f'Syntax error at {p.value}')
 
@@ -143,39 +155,72 @@ def parse_yaracondition_for_logic_block(parse_target):
 
     return [ele[0] for ele in sorted(var_lex_pos, key=lambda x:x[1])], ast
 
+
 def cleanup():
     filedir = os.path.dirname(os.path.abspath(__file__))
     try:
         os.unlink(os.path.join(filedir, 'parsetab.py'))
-    except:
+    except OSError:
         pass
     try:
         os.unlink(os.path.join(filedir, 'parser.out'))
-    except:
+    except OSError:
         pass
+
 
 def test():
     # case 1
     case1 = '( ( $abcdefg and $b ) or ( $c and $d ) ) and not ( $e )'
     result1 = parse_yaracondition_for_logic_block(case1)
     assert result1[0] == ['$abcdefg', '$b', '$c', '$d', '$e']
-    assert result1[1] == ['(', '(', '$abcdefg', 'and', '$b', ')', 'or', '(', '$c', 'and', '$d', ')', ')', 'and', 'not', '( $e )']
-    
+    assert result1[1] == [
+        '(', '(', '$abcdefg', 'and', '$b', ')',
+        'or',
+        '(', '$c', 'and', '$d', ')', ')', 'and', 'not', '( $e )'
+        ]
+
     # case 2
-    case2 = '( $a in ( 1 .. 100 ) or ( #b * 2 ) != 6 or ( $c > 100 and !d[1] == 2 ) )'
+    case2 = '( $a in ( 1 .. 100 ) or ( #b * 2 ) != 6' +\
+            ' or ' +\
+            '( $c > 100 and !d[1] == 2 ) )'
     result2 = parse_yaracondition_for_logic_block(case2)
     assert result2[0] == ['$a', '#b', '$c', '!d']
-    assert result2[1] == ['(', '$a in ( 1 .. 100 )', 'or', '( #b * 2 ) != 6', 'or', '(', '$c > 100', 'and', '!d[1] == 2', ')', ')']
-    
+    assert result2[1] == [
+        '(', '$a in ( 1 .. 100 )',
+        'or', '( #b * 2 ) != 6',
+        'or',
+        '(', '$c > 100', 'and', '!d[1] == 2', ')',
+        ')'
+        ]
+
     # case 3
     case3 = '1 of ( $* )'
     result3 = parse_yaracondition_for_logic_block(case3)
     assert result3[0] == ['$*']
     assert result3[1] == ['1 of ( $* )']
-    
+
     # case 4
-    case4 = '( ( for any of ( $a, $b, $c ) : ( $ ) and ( $d ) ) or ( for all of ( $g* ) : ( @ > @h ) ) )'
+    case4 = '( ( for any of ( $a, $b, $c ) : ( $ ) and ( $d ) )' +\
+            ' or ' +\
+            '( for all of ( $g* ) : ( @ > @h ) ) )'
     result4 = parse_yaracondition_for_logic_block(case4)
     assert result4[0] == ['$a', '$b', '$c', '$', '$d', '$g*', '@', '@h']
-    assert result4[1] == ['(', '(', 'for', 'any of ( $a , $b , $c )', ':', '( $ )', 'and', '( $d )', ')', 'or', '(', 'for', 'all of ( $g* )', ':', '( @ > @h )', ')', ')']
-
+    assert result4[1] == [
+        '(',
+        '(',
+        'for',
+        'any of ( $a , $b , $c )',
+        ':',
+        '( $ )',
+        'and',
+        '( $d )',
+        ')',
+        'or',
+        '(',
+        'for',
+        'all of ( $g* )',
+        ':',
+        '( @ > @h )',
+        ')',
+        ')'
+        ]
--- print_vaddr_from_yara_result.py
+++ print_vaddr_from_yara_result.py
@@ -4,8 +4,9 @@ import argparse
 import glob
 
 import yara
-from elftools.elf.elffile import ELFFile
-from pefile import PE
+from elftools.elf.elffile import ELFFile, ELFError
+from pefile import PE, PEFormatError
+
 
 class AnalyzeObject:
     def __init__(self, analyzeobj):
@@ -14,13 +15,19 @@ class AnalyzeObject:
     def get_sectionname_and_virtualaddress(self, offset):
         raise NotImplementedError
 
+
 class PEObject(AnalyzeObject):
     def get_sectionname_and_virtualaddress(self, offset):
         for section in self.analyzeobj.sections:
             if section.contains_offset(offset):
-                return (section.Name.decode().strip('\x00'), self.analyzeobj.OPTIONAL_HEADER.ImageBase + section.get_rva_from_offset(offset))
+                return (
+                    section.Name.decode().strip('\x00'),
+                    self.analyzeobj.OPTIONAL_HEADER.ImageBase +
+                    section.get_rva_from_offset(offset)
+                    )
         else:
             return ('', -1)
+
 
 class ELFObject(AnalyzeObject):
     def get_sectionname_and_virtualaddress(self, offset):
@@ -28,24 +35,29 @@ class ELFObject(AnalyzeObject):
             secoffset = section['sh_offset']
             secsize = section['sh_size']
             if offset >= secoffset and offset < secoffset + secsize:
-                return (section.name, (offset - secoffset) + section['sh_addr'])
+                return (
+                    section.name,
+                    (offset - secoffset) + section['sh_addr']
+                    )
         else:
             return ('', -1)
+
 
 def detect_filetype(fd):
     try:
         pe = PEObject(PE(fd.name))
         return pe
-    except:
+    except (PEFormatError, OSError):
         pass
 
     try:
         elf = ELFObject(ELFFile(fd))
         return elf
-    except:
+    except (ELFError, OSError):
         pass
 
     return None
+
 
 def get_yara_rule(rulefilename):
     try:
@@ -53,13 +65,15 @@ def get_yara_rule(rulefilename):
     except (yara.SyntaxError, yara.Error) as e:
         print(e)
         return None
-    except:
+    except OSError:
         print('error occured(get_yara_rule(%s))' % rulefilename)
         return None
     return result
 
+
 def check_yararule(fd, yararule):
     return yararule.match(fd.name)
+
 
 def main(filename, rulefilename=None, allrule=None):
     try:
@@ -67,43 +81,70 @@ def main(filename, rulefilename=None, allrule=None):
     except (FileNotFoundError, IsADirectoryError, PermissionError) as e:
         print(e)
         sys.exit(0)
-    except:
+    except OSError:
         print('error occured(main(%s, %s))' % (filename, rulefilename))
         sys.exit(0)
 
     analyzeobj = detect_filetype(fd)
-    if analyzeobj == None:
+    if analyzeobj is None:
         fd.close()
         return None
 
     rulefilenames = []
     if allrule:
-        for rulefilename in glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'yararules', '*', '*.yar')):
+        for rulefilename in glob.glob(
+                os.path.join(
+                    os.path.dirname(
+                        os.path.abspath(__file__)
+                    ),
+                    'yararules', '*', '*.yar'
+                )
+        ):
             rulefilenames.append(rulefilename)
-    elif rulefilename == None:
-        rulefilenames.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'yararules', 'rules.yar'))
+    elif rulefilename is None:
+        rulefilenames.append(
+            os.path.join(
+                os.path.dirname(
+                    os.path.abspath(__file__)
+                ),
+                'yararules', 'rules.yar'
+            )
+        )
     else:
         rulefilenames = [os.path.expanduser(rulefilename)]
 
     resultdict = {}
-    # Note: one by one processing is slow (yara matching uses Aho-Corasick algorithm)
+    # Note: one by one processing is slow
+    # (yara matching uses Aho-Corasick algorithm)
     for rulefilename in rulefilenames:
         yararule = get_yara_rule(rulefilename)
-        if yararule == None:
+        if yararule is None:
             continue
         yararesult = check_yararule(fd, yararule)
         result_each_rule = {}
         for yararesultele in yararesult:
             result_each_rule_each_string = []
             for yararesultelestringele in yararesultele.strings:
-                secname, vaddr = analyzeobj.get_sectionname_and_virtualaddress(yararesultelestringele[0])
-                result_each_rule_each_string.append({"offset":yararesultelestringele[0], "string_identifier":yararesultelestringele[1], "string_data":yararesultelestringele[2], "section_name":secname, "vaddr":vaddr})
-            result_each_rule[(yararesultele.rule, yararesultele.meta['description'])] = result_each_rule_each_string
+                secname, vaddr = analyzeobj.get_sectionname_and_virtualaddress(
+                        yararesultelestringele[0]
+                    )
+                result_each_rule_each_string.append({
+                        "offset": yararesultelestringele[0],
+                        "string_identifier": yararesultelestringele[1],
+                        "string_data": yararesultelestringele[2],
+                        "section_name": secname,
+                        "vaddr": vaddr
+                    })
+            result_each_rule[(
+                    yararesultele.rule,
+                    yararesultele.meta['description']
+                )] = result_each_rule_each_string
         if result_each_rule != {}:
             resultdict[os.path.basename(rulefilename)] = result_each_rule
 
     fd.close()
     return resultdict
+
 
 def view(result):
     for rulefilename, result_each_rule in result.items():
@@ -112,22 +153,60 @@ def view(result):
             print(f'\t{rule[0]}("{rule[1]}"):')
             for each_string in string:
                 if len(each_string["string_data"]) <= 10:
-                    stringhex = ' '.join([hex(c)[2:].zfill(2) for c in each_string["string_data"]])
+                    stringhex = ' '.join([
+                        hex(c)[2:].zfill(2)
+                        for c in each_string["string_data"]
+                    ])
                 else:
-                    stringhex = ' '.join([hex(c)[2:].zfill(2) for c in each_string["string_data"][:10]])+' ...'
+                    stringhex = ' '.join([
+                        hex(c)[2:].zfill(2)
+                        for c in each_string["string_data"][:10]
+                        ]) + ' ...'
                 if each_string["vaddr"] > 0:
-                    print(f'\t\toffset={hex(each_string["offset"])}(vaddr={hex(each_string["vaddr"])}, section={each_string["section_name"]}):{each_string["string_identifier"]}: {stringhex}')
+                    print(
+                        '\t\toffset=' +
+                        f'{hex(each_string["offset"])}' +
+                        '(' +
+                        f'vaddr={hex(each_string["vaddr"])}, ' +
+                        f'section={each_string["section_name"]}' +
+                        '):' +
+                        f'{each_string["string_identifier"]}: {stringhex}'
+                    )
                 else:
-                    print(f'\t\toffset={hex(each_string["offset"])}(no info):{each_string["string_identifier"]}: {stringhex}')
+                    print(
+                        '\t\toffset=' +
+                        f'{hex(each_string["offset"])}' +
+                        '(no info):' +
+                        f'{each_string["string_identifier"]}: {stringhex}'
+                    )
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="yara matching and print virtual address for matching string")
+    parser = argparse.ArgumentParser(
+        description=""
+        "yara matching and print virtual address for matching string"
+        )
 
-    parser.add_argument("filename", help="filename for the binary of analyzing target")
-    parser.add_argument("--rulefilename", help="filename for the yara file which includes specific yara rules")
-    parser.add_argument("--allrule", help="flag for processing all defined yara rules one by one (slow method)", action='store_true')
+    parser.add_argument(
+        "filename",
+        help="filename for the binary of analyzing target"
+        )
+    parser.add_argument(
+        "--rulefilename",
+        help="filename for the yara file which includes specific yara rules"
+        )
+    parser.add_argument(
+        "--allrule",
+        help="flag for processing all defined yara rules one by one "
+        "(slow method)",
+        action='store_true'
+        )
 
     args = parser.parse_args()
 
-    result = main(args.filename, rulefilename=args.rulefilename, allrule=args.allrule)
+    result = main(
+        args.filename,
+        rulefilename=args.rulefilename,
+        allrule=args.allrule
+        )
     view(result)
